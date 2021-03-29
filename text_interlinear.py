1cbc07bbf2dc35c5e5fab5364eb4f97469bc3446
steve <stevetasticsteve@gmail.com>
2021-03-29 17:06:16 +0100

    Removed bootstrap, used plain css

--- text_interlinear.py
+++ text_interlinear.py
@@ -9,7 +9,7 @@ import settings
 
 
 def read_wordlist():
-    with open(settings.word_list_file, 'r', encoding='utf8') as csvfile:
+    with open(settings.word_list_file, "r", encoding="utf8") as csvfile:
         reader = csv.reader(csvfile)
         words = [str(w).lstrip("'[").rstrip("]'") for w in reader]
     return words
@@ -25,24 +25,24 @@ def parse_xml(xml_file):
     for t in list(data):
         text = {}
         try:
-            text['title'] = t.get('title').strip()
+            text["title"] = t.get("title").strip()
         except AttributeError:  # It's none
-            text['title'] = ''
+            text["title"] = ""
         for elem in t:
             try:
                 text[elem.tag] = elem.text.strip()
             except AttributeError:  # It's none
-                text[elem.tag] = ''
+                text[elem.tag] = ""
 
-        if not text['kovol']:  # Use the matat if no imengis is available
-            if text['matat']:
-                text['kovol_interlinear'] = text['matat'].strip().split(' ')
+        if not text["kovol"]:  # Use the matat if no imengis is available
+            if text["matat"]:
+                text["kovol_interlinear"] = text["matat"].strip().split(" ")
         else:
-            text['kovol_interlinear'] = text['kovol'].strip().split(' ')
-        text['english_interlinear'] = text['literal'].strip().split(' ')
-        
+            text["kovol_interlinear"] = text["kovol"].strip().split(" ")
+        text["english_interlinear"] = text["literal"].strip().split(" ")
+
         texts.append(text)
-        
+
     return texts
 
 
@@ -50,40 +50,41 @@ def html_it(texts, user):
     # Use a Jinja template to create the web page
     file_loader = FileSystemLoader(settings.code_folder)
     env = Environment(loader=file_loader, autoescape=True)
-    template = env.get_template('text_template.j2')
-    file_name = user + '_texts.html'
+    template = env.get_template("text_template.j2")
+    file_name = user + "_texts.html"
     html_file = os.path.join(settings.html_folder, file_name)
-    with open(html_file, 'w') as file:
+    with open(html_file, "w") as file:
         print(template.render(texts=texts, wordlist=wordlist), file=file)
-    print('HTML created for {user}'.format(user=file_name))
+    print("HTML created for {user}".format(user=file_name))
 
 
 def create_index():
-# Create a basic index page for Apache to use
-    with open(os.path.join(settings.html_folder, 'index.html'), 'w') as index_file:
+    # Create a basic index page for Apache to use
+    with open(os.path.join(settings.html_folder, "index.html"), "w") as index_file:
         index_file.write('<div class="container">')
         for file in os.listdir(settings.html_folder):
-            if file == 'index.html':
+            if file == "index.html":
                 continue
-            if file.endswith('.html'):
+            if file.endswith(".html"):
                 index_file.write(
-                    '<h3><li><a href="{filename}">{page}</a></li></h3>'.format(filename=file, page=file))
-        index_file.write('</div>')
+                    '<h3><li><a href="{filename}">{page}</a></li></h3>'.format(
+                        filename=file, page=file
+                    )
+                )
+        index_file.write("</div>")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     wordlist = read_wordlist()
     for xml in os.listdir(settings.xml_folder):
-    # don't bother with the user guide to the xml scheme
-        if xml == 'text_xml_schema.md':
+        # don't bother with the user guide to the xml scheme
+        if not xml.endswith(".xml"):
             continue
-        elif xml == 'audio':
-            continue
-        user = os.path.basename(xml).rstrip('.xml')
+        user = os.path.basename(xml).rstrip(".xml")
         try:
-            texts = parse_xml(os.path.join(settings.xml_folder, xml))
+            text_file = os.path.join(settings.xml_folder, xml)
+            texts = parse_xml(text_file)
             html_it(texts, user)
         except Exception as e:
-            print("Error: " + e)
+            print("Error: " + str(e))
     create_index()
-
